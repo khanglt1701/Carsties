d3c9d55bc4512a18772eaafe8c16b3d774875cf9
Khang Le Tuong <khanglt1701@gmail.com>
2024-10-08 14:11:50 +0700

    Refactoring our code to use the zustand state

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,14 +1,13 @@
 import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'
-
-type Props = {
-  pageSize: number,
-  setPageSize: (size: number) => void
-}
+import { useParamsStore } from '../hooks/useParamsStore';
 
 const pageSizeButton = [4, 8, 12];
 
-function Filters({pageSize, setPageSize}: Props) {
+function Filters() {
+  const pageSize = useParamsStore(state => state.pageSize);
+  const setParams = useParamsStore(state => state.setParams);
+
   return (
     <div className='flex justify-between items-center mb-4'>
       <div>
@@ -16,7 +15,7 @@ function Filters({pageSize, setPageSize}: Props) {
         <ButtonGroup>
           {pageSizeButton.map((value, index) => (
             <Button key={index} 
-              onClick={() => setPageSize(value)} 
+              onClick={() => setParams({pageSize: value})} 
               color={`${pageSize === value ? 'red' : 'gray'}`}
             >
               {value}
