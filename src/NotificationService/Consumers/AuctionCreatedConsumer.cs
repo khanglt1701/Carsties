95587589535f0941dcf8ba1ee3e5119f4ca4c7ec
Khang Le Tuong <khanglt1701@gmail.com>
2024-11-14 16:00:15 +0700

    end of adding NotificationService

---
+++ src/NotificationService/Consumers/AuctionCreatedConsumer.cs
@@ -0,0 +1,18 @@
+using System;
+using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers;
+
+public class AuctionCreatedConsumer(IHubContext<NotificationHub> hubContext) : IConsumer<AuctionCreated>
+{
+    private readonly IHubContext<NotificationHub> _hubContext = hubContext;
+
+    public async Task Consume(ConsumeContext<AuctionCreated> context)
+    {
+        Console.WriteLine("--> Auction created messsage received");
+        await _hubContext.Clients.All.SendAsync("AuctionCreated", context.Message);
+    }
+}
