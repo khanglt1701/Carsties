ec6b1e8bfa7d060b0d2a3e6e2b767f2fc0b62775
Khang Le Tuong <khanglt1701@gmail.com>
2024-11-14 16:51:18 +0700

    Creating a bid item

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -7,6 +7,7 @@ import DetailedSpecs from './DetailedSpecs';
 import { getCurrentUser } from '@/app/actions/authAction';
 import EditButton from './EditButton';
 import DeleteButton from './DeleteButton';
+import BidItem from './BidItem';
 
 async function Details({params}: {params: {id: string}}) {
   const data = await getDetailedViewData(params.id);
@@ -40,7 +41,7 @@ async function Details({params}: {params: {id: string}}) {
         <div className="border-2 rounded-lg p-2 bg-gray-100">
           <Heading title='Bids' /> 
           {bids.map(bid => (
-            <p key={bid.id}>{bid.bidder} - {bid.amount}</p>
+            <BidItem key={bid.id} bid={bid} />
           ))}
         </div>
       </div>
